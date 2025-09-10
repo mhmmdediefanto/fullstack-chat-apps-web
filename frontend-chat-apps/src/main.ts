c79aa5aeeabf5749be5020ff26d8f201b2ff09ec
mhmmdediefanto <ediefanto10@gmail.com>
2025-09-10 18:16:13 +0700

    first commit

--- frontend-chat-apps/src/main.ts
+++ frontend-chat-apps/src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import "@/main.css"
 
 import App from './App.vue'
 import router from './router'
+import './main.css'
 
 const app = createApp(App)
 
