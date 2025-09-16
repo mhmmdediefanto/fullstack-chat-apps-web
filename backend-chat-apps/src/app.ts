11acdd80e31f16b0a4e2f20f0c7eef4701ae8375
mhmmdediefanto <ediefanto10@gmail.com>
2025-09-16 13:09:49 +0700

    update cors origin

--- backend-chat-apps/src/app.ts
+++ backend-chat-apps/src/app.ts
@@ -4,6 +4,7 @@ import ContactRoute from "./routes/ContactRoutes";
 import ChatRoute from "./routes/ChatRoutes";
 import GroupRoute from "./routes/GroupRoutes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app: Application = express();
 
@@ -11,6 +12,16 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
+// setting cors origin
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Routes
 app.use("/api/v1/auth", AuthRoute);
--- backend-chat-apps/src/index.ts
+++ backend-chat-apps/src/index.ts
@@ -2,6 +2,8 @@ import http from "http";
 import app from "./app";
 import { Server } from "socket.io";
 import registerSockets from "./sockets";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,8 +11,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*", // ganti sesuai domain frontend kamu
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", // ganti sesuai domain frontend kamu
     methods: ["GET", "POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   },
 });
 
